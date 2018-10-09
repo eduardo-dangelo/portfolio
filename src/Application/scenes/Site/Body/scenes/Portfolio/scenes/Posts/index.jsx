f723273ba336764fb053450860c86706fe33adc5
eduardo-dangelo <eduardodonatodangelo@gmail.com>
2018-10-09 21:08:32 +0100

    Start building post page

--- src/Application/scenes/Site/Body/scenes/Portfolio/scenes/Posts/index.jsx
+++ src/Application/scenes/Site/Body/scenes/Portfolio/scenes/Posts/index.jsx
@@ -1,18 +1,52 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const PostContainer = styled.div`
+const PostsContainer = styled.div`
   background: #d9d7d4;
-  padding: 15px;
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
   flex: 1 0 auto;
+`;
+
+const PostItem = styled.div`
+  width: 25%;
+  height: 300px;
+  border: 1px solid orange;
+  background: gray;
+  opacity: 1;
+  
+  &:hover {
+    opacity: .6;
+  }
 `;
 
 class Posts extends React.PureComponent {
   render() {
     return (
-      <PostContainer>
-        <h2>Posts here</h2>
-      </PostContainer>
+      <PostsContainer>
+        <PostItem>
+          some title
+        </PostItem>
+        <PostItem>
+          some title
+        </PostItem>
+        <PostItem>
+          some title
+        </PostItem>
+        <PostItem>
+          some title
+        </PostItem>
+        <PostItem>
+          some title
+        </PostItem>
+        <PostItem>
+          some title
+        </PostItem>
+        <PostItem>
+          some title
+        </PostItem>
+      </PostsContainer>
     )
   }
 }
