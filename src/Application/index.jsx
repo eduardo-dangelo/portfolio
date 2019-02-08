255edf431cc3a5d8ee3d859d10bf55b4ebc2542e
eduardo-dangelo <eduardodonatodangelo@gmail.com>
2019-02-08 22:08:11 +0000

    add redirect

--- src/Application/index.jsx
+++ src/Application/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Site from './scenes/Site';
 import AdminBar from './scenes/AdminBar';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
 
 class Application extends React.Component {
   render() {
     return (
       <Router basename={process.env.PUBLIC_URL}>
         <div>
+          <Redirect from='/og_img.png' to='/'/>
           <Route path='/admin' render={() => <AdminBar/>}/>
           <Route path='/' render={() => <Site/>} />
         </div>
