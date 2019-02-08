32a4a211b5ecfd50f2efefb055a074fcf19a0c38
eduardo-dangelo <eduardodonatodangelo@gmail.com>
2019-02-08 22:12:44 +0000

    fix router

--- src/Application/index.jsx
+++ src/Application/index.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import Site from './scenes/Site';
 import AdminBar from './scenes/AdminBar';
-import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 
 class Application extends React.Component {
   render() {
     return (
       <Router basename={process.env.PUBLIC_URL}>
-        <div>
+        <Switch>
           <Redirect from='/og_img.png' to='/'/>
-          <Route path='/admin' render={() => <AdminBar/>}/>
-          <Route path='/' render={() => <Site/>} />
-        </div>
+          <Route path='/admin' component={AdminBar}/>
+          <Route path='/' component={Site} />
+        </Switch>
       </Router>
     );
   }
